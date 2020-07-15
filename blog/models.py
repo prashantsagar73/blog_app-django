250e24db400545750840d3f929cf5a1fbba3bc09
prashant <prashant.sagar73@gmail.com>
2020-07-15 15:13:02 +0530

    blog page completed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    sno=models.AutoField(primary_key=True)
+    title= models.CharField(max_length=200)
+    content= models.TextField()
+    author= models.CharField(max_length=50)    
+    slug= models.CharField(max_length=150)    
+    timeStamp= models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title + 'by' +self.author
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import Post
 
 
 # Create your views here.
 def blogHome(request):
-    return render(request,'blog/bloghome.html')
+    allPosts = Post.objects.all()
+    # print(allPosts)
+    context = {'allPosts' : allPosts}
+    return render(request,'blog/bloghome.html',context)
     # return HttpResponse('this is blogHome. all the blog post will be here')
 def blogPost(request,slug):
-    return render(request,'blog/blogpost.html')
+    post = Post.objects.filter(slug=slug).first()
+    context = {'post':post}
+    return render(request,'blog/blogpost.html',context)
 
     # return HttpResponse(f'this is blogPost{slug}')    
  
