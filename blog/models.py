db797efcce75e8dd81877a97df24d08375474257
prashant <prashant.sagar73@gmail.com>
2020-07-17 15:37:14 +0530

    coment model and basic frame of commnet

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
 
 # Create your models here.
 class Post(models.Model):
@@ -11,3 +14,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + 'by' +self.author
+         
+class BlogComment(models.Model):
+    sno=models.AutoField(primary_key=True)
+    comment= models.TextField(max_length=400)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)    
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,null=True)    
+    timestamp =models.DateTimeField(default=now)
+
+
+
+            
+ 
