27915ac6732ac53f6fc1d40a558623157e9dfa95
prashant <prashant.sagar73@gmail.com>
2020-07-18 23:45:12 +0530

    coment reply added but not working -- try to solve it later

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class BlogComment(models.Model):
     comment= models.TextField(max_length=400)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)    
-    parent = models.ForeignKey('self', on_delete=models.CASCADE,null=True)    
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)    
     timestamp =models.DateTimeField(default=now)
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from blog.models import Post,BlogComment
 from django.contrib import messages
+# from blog.templatetags import extras
 
 
 # Create your views here.
@@ -12,8 +13,16 @@ def blogHome(request):
     # return HttpResponse('this is blogHome. all the blog post will be here')
 def blogPost(request,slug):
     post = Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(post=post)
-    context = {'post':post, 'comments':comments,'user':request.user}
+    comments = BlogComment.objects.filter(post=post, parent=None)
+    replies = BlogComment.objects.filter(post=post).exclude(parent=None)
+    replyDict={}
+    for reply in replies:
+        if reply.parent.sno not in replyDict.keys():
+            replyDict[reply.parent.sno]= [reply]
+        else:
+            replyDict[reply.parent.sno].append(reply) 
+              
+    context = {'post':post, 'comments':comments,'user':request.user,'replydict': replyDict}
     return render(request,'blog/blogpost.html',context)
 
     # return HttpResponse(f'this is blogPost{slug}')
@@ -24,10 +33,17 @@ def postComment(request):
         user = request.user
         postSno = request.POST.get("postSno")   
         post = Post.objects.get(sno=postSno)
+        parentSno = request.POST.get("parentSno")
+        if parentSno == "":
+            comment = BlogComment(comment=comment, user=user, post=post)
+            comment.save()
+            messages.success(request,"Your comment has been posted successfully")
+        else:
+            parent = BlogComment.objects.get(sno=parentSno)
+            comment = BlogComment(comment=comment, user=user, post=post,parent=parent)
 
-        comment = BlogComment(comment=comment, user=user, post=post)
-        comment.save()
-        messages.success(request,"Your comment has been posted successfully")
+            comment.save()
+            messages.success(request,"Your Reply has been posted successfully")
 
     return redirect(f"/blog/{post.slug}")       
  
