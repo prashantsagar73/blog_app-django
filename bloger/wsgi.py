7603cc0738d8b14be02ff86e5b5c554203431756
Prashant sagar <prashant.sagar73@gmail.com>
2021-05-07 01:50:01 +0530

    requirement updated

--- bloger/wsgi.py
+++ bloger/wsgi.py
@@ -8,11 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-# from dj_static import Cling
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloger.settings')
 
 # application = Cling(get_wsgi_application())
 application = get_wsgi_application()
+application = Cling(get_wsgi_application())
